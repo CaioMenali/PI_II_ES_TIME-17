35c1c34db15b1cf3ff0eb92e45bd3145c6d2560c
Felipe Lirani <fc.lirani@gmail.com>
2025-10-07 10:56:12 -0300

    Delete Scripts directory

--- Scripts/login/login.js
+++
@@ -1,9 +0,0 @@
-function try_login() {
-    // Funcao de login
-    const email = document.getElementById('email').value;
-    const password = document.getElementById('password').value;
-
-    alert(`Email: ${email}\nPassword: ${password}`);
-
-    window.location.href = '/pages/homepage.html';
-}
