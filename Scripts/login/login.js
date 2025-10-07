5ae9e1d1134a10e2b6b88faed14093e917c90dd7
Felipe Lirani <fc.lirani@gmail.com>
2025-10-07 10:54:46 -0300

    Add files via upload

---
+++ Scripts/login/login.js
@@ -0,0 +1,9 @@
+function try_login() {
+    // Funcao de login
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
+
+    alert(`Email: ${email}\nPassword: ${password}`);
+
+    window.location.href = '/pages/homepage.html';
+}
